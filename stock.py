9790322e12fb91a6c6faf0dbfcab7eb83f25865b
Bruno Sampietro <bsampietro@gmail.com>
2017-09-15 21:50:56 -0300

    Calculate HV in a new method

--- stock.py
+++ stock.py
@@ -5,6 +5,10 @@ from util import *
 import statistics
 import math
 
+def calculate_hv(closes):
+    # return (statistics.stdev(closes) / closes[-1]) * 100 * math.sqrt(252/len(closes))
+    return (statistics.stdev(closes) / statistics.mean(closes)) * 100 * math.sqrt(252/len(closes))
+
 class Stock:
     def __init__(self, data_handler, ticker):
         self.data_handler = data_handler
@@ -12,8 +16,10 @@ class Stock:
 
 
     def hv(self, back_days):
-        closes = self.closes(back_days)
+        return calculate_hv(self.closes(back_days))
 
+
+    # def percentage_hv(self, back_days):
         # nr_of_closes = len(closes)
         # percentage_changes = []
         # for i in range(nr_of_closes):
@@ -22,18 +28,14 @@ class Stock:
         #     percentage_changes.append((closes[i] / closes[i-1] - 1) * 100)
         # return statistics.stdev(percentage_changes) * math.sqrt(365/back_days)
 
-        # return (statistics.stdev(closes) / self.ma(back_days)) * 100 * math.sqrt(365/back_days)
-
-        return (statistics.stdev(closes) / closes[-1]) * 100 * math.sqrt(365/back_days)
-
 
     @lru_cache(maxsize=None)
     def period_hvs(self):
         closes = self.closes(365)
         hvs = []
-        for i in range(len(closes) - 30):
-            monthly_closes = closes[i:i+30]
-            hvs.append((statistics.stdev(monthly_closes) / monthly_closes[-1]) * 100 * math.sqrt(12))
+        for i in range(len(closes) - 21):
+            monthly_closes = closes[i:i+21]
+            hvs.append(calculate_hv(monthly_closes))
         return hvs
 
 
